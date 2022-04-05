ce5ba4f4a7083db94f93b74b4ef17bfb764edbc1
Guilherme Faga <guilherme.galletti@dp6.com.br>
2022-04-05 15:41:03 -0300

    update gps_server

--- Autominer/gps_server/main.lua
+++ Autominer/gps_server/main.lua
@@ -1,55 +1,21 @@
 rednet.open("back")
 rednet.host("gps_server", "master")
 
-local PATH = "data.txt"
-
+local PATH = "logs/"
 
 function main()
   print("Listening for GPS data on protocol 'gps_server'...")
   while true do
     event, senderId, message, protocol = os.pullEvent("rednet_message")
     if protocol == "gps_server" then
-      local x, y, z = split(message, " ")
       
-      local file = fs.open(PATH, "r")
-      local table = {}
-
-      while true do
-        local line = file.readLine()
-        if not line then break end
-
-        str = split(line, "=")
-        key = str[1]
-        value = str[2]
-        table[key] = value
-      end
-      file.close()
-
+      local file = fs.open(PATH..senderId..".txt", "w")
       print("Saving GPS data for sender "..senderId.."...")
-      table[senderId+""] = message
-
-      writeTableToFile(table, PATH)
+      file.writeLine(message)
+      file.close()
+      print("Done.")
     end
   end
-
-  function writeTableToFile(table, path)
-    fs.delete(path)
-    local file = fs.open(path, "w")
-    for key, value in pairs(table) do
-      file.writeLine(key.."="..value)
-    end
-  end
-end
-
-function split (inputstr, sep)
-  if sep == nil then
-     sep = "%s"
-  end
-  local t={}
-  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-     table.insert(t, str)
-  end
-  return t
 end
 
 main{...}
